bd51cf3e25c4846b473585ae72a8459539778d9d
Evyatar Yotvat <Evyatar431@gmail.com>
2025-12-02 23:15:03 +0200

    Commit 2 - updated HTML, CSS and JS for car form client

---
+++ script.js
@@ -0,0 +1,13 @@
+$("#carForm").on("submit", function (e) {
+    e.preventDefault();
+
+    const brand = $("#brand").val();
+    const model = $("#model").val();
+
+    $.post("http://localhost:3000/api/cars", 
+        { brand, model },
+        function(response) {
+            $("#response").text(response);
+        }
+    );
+});
