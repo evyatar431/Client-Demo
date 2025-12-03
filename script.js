d81343b9a0072d5086005ce50db34cf5df6dabac
noammiz1236 <noammizrahi2002@gmail.com>
2025-12-03 14:24:10 +0200

    Change comments

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 $("#carForm").on("submit", function (e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    const brand = $("#brand").val();
-    const model = $("#model").val();
+  const brand = $("#brand").val();
+  const model = $("#model").val();
 
-$.post("http://localhost:3000/api/cars", 
+  $.post(
+    "http://localhost:3000/api/cars",
     { brand, model },
-    function(response) {
-        $("#response").text(response);
+    function (response) {
+      $("#response").text(response);
     }
-);
-
+  );
 });
+// Check woir
